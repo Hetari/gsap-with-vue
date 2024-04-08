df253f463c1cb2acbbf945031a4007ed48b0d656
Ebraheem Alhetari <hetari4py@gmail.com>
2024-04-08 03:55:40 +0300

    Implement the gsap.fromTo() method

--- src/views/GsapFromTo.vue
+++ src/views/GsapFromTo.vue
@@ -29,7 +29,7 @@
     </p>
 
     <div class="mt-20">
-      <div id="red-box" class="w-20 h-20 bg-red-500 rounded-lg" />
+      <div ref="box" id="red-box" class="w-20 h-20 bg-red-500 rounded-lg" />
     </div>
     <div class="mt-20 text-end">
       <Button />
@@ -38,6 +38,32 @@
 </template>
 
 <script setup lang="ts">
-// TODO: Implement the gsap.fromTo() method
 import { Button } from "@/components";
+// TODO:
+// [x] Implement the gsap.fromTo() method
+import { ref, onMounted, type Ref } from "vue";
+
+// TODO:
+// [x] Implement the gsap.fromTo() method
+import { gsap } from "gsap";
+const box: Ref<null> = ref(null);
+onMounted(() => {
+  gsap.fromTo(
+    box.value,
+    {
+      yPercent: 150,
+      borderRadius: 0,
+    },
+    {
+      xPercent: 150,
+      yPercent: 0,
+      rotate: 105,
+      borderRadius: 50,
+      duration: 2.5,
+      ease: "expo.inOut",
+      repeat: -1,
+      yoyo: true,
+    }
+  );
+});
 </script>
